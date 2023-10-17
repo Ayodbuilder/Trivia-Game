94829843093b177d5acb1e1d14a9a3bfaa769c32
Victor Ayobami Kehinde <143555229+Ayodbuilder@users.noreply.github.com>
2023-10-17 22:49:00 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,111 +1,111 @@
 const questions = [
     {
-        question: "What is the capital of France?",
+        question: "1. What is the capital of France?",
         options: ["London", "Berlin", "Madrid", "Paris"],
         answer: "Paris"
     },
     {
-        question: "Which planet is known as the Red Planet?",
+        question: "2. Which planet is known as the Red Planet?",
         options: ["Mars", "Earth", "Jupiter", "Venus"],
         answer: "Mars"
     },
     {
-        question: "What is the largest mammal in the world?",
+        question: "3. What is the largest mammal in the world?",
         options: ["Elephant", "Blue Whale", "Giraffe", "Lion"],
         answer: "Blue Whale"
     },
     {
-        question: "What color does gold leaf appear if you hold it up to the light?",
+        question: "4. What color does gold leaf appear if you hold it up to the light?",
         options: ["Gold", "Yellow", "Green", "ROYGBIV"],
         answer: "Green"
     },
     {
-        question: "Which instrument is associated with Earl 'Bud' Powell?",
+        question: "5. Which instrument is associated with Earl 'Bud' Powell?",
         options: ["Guitar", "Drum set", "Piano", "Saxophone"],
         answer: "Piano"
     },
     {
-        question: "What is the most popular drink in the world that isn't alcohol?",
+        question: "6. What is the most popular drink in the world that isn't alcohol?",
         options: ["Water", "Coffee", "CocaCola", "Fruit Wine"],
         answer: "Coffee"
     },
     {
-        question: "Name 2 animals that sleeps with their eyes opened",
+        question: "7. Name 2 animals that sleeps with their eyes opened",
         options: ["Bear/Horse", "Porcupine/Jaguar", "Hen/Ostrich", "Fish/Snake"],
         answer: "Fish/Snake"
     },
     {
-        question: "What animal is capable of sleeping for up to 3 years?",
+        question: "8. What animal is capable of sleeping for up to 3 years?",
         options: ["Cats", "Snails", "Polar Bears", "Blue Whale"],
         answer: "Snails"
     },
     {
-        question: "Name the only part of the humsn body that is fully grown from birth",
+        question: "9. Name the only part of the human body that is fully grown from birth",
         options: ["The Hairs", "The Tongue", "The Eyeball", "The Nose"],
         answer: "The Eyeball"
     },
     {
-        question: "What does it mean to feel blue?",
+        question: "10. What does it mean to feel blue?",
         options: ["To be happy", "To be scared", "To be excited", "To be sad"],
         answer: "To be sad"
     },
     {
-        question: "What is a female donkey called?",
+        question: "11. What is a female donkey called?",
         options: ["A Jennet", "A Silvia", "A Nanny", "A Nancy"],
         answer: "A Jennet"
     },
     {
-        question: "What was the first movie to be rated PG-13?",
+        question: "12. What was the first movie to be rated PG-13?",
         options: ["Apocalypto", "Power Rangers", "Red Dawn", "Daredevil"],
         answer: "Red Dawn"
     },
     {
-        question: "If you order “murgh” from the menu at an Indian restaurant, what meat will you get?",
+        question: "13. If you order “murgh” from the menu at an Indian restaurant, what meat will you get?",
         options: ["Beef", "Pork", "Chicken", "Mutton"],
         answer: "Chicken"
     },
     {
-        question: "Among land animals, what species has the largest eyes?",
+        question: "14. Among land animals, what species has the largest eyes?",
         options: ["Cats", "Hippopotamus", "Ostrich", "Turkey"],
         answer: "Ostrich"
     },
     {
-        question: "So far, which continent has hosted the Olympics the most times?",
+        question: "15. So far, which continent has hosted the Olympics the most times?",
         options: ["Africa", "Asia", "Europe", "Antarctica"],
         answer: "Europe"
     },
     {
-        question: " In 1612, who became the first person to observe the planet Neptune?",
+        question: "16. In 1612, who became the first person to observe the planet Neptune?",
         options: ["Neil Armstrong", "Galileo", "Isaac Newton", "Michael Faraday"],
         answer: "Galileo"
     },
     {
-        question: "What is the world’s most venomous fish?",
+        question: "17. What is the world’s most venomous fish?",
         options: ["Stonefish", "Jelly fish", "Puffer fish", "Titus"],
         answer: "   Stonefish"
     },
     {
-        question: "Emetophobia is the fear of?",
+        question: "18. Emetophobia is the fear of?",
         options: ["Death", "Vomit", "Phones", "Sickness"],
         answer: "Vomit"
     },
     {
-        question: "What insect has the shortest life span?",
+        question: "19. What insect has the shortest life span?",
         options: ["Tse-Tse flies", "Mayflies", "Mosquitoes", "Housefly"],
         answer: "Mayfles"
     },
     {
-        question: "If Teresa's daughter is my daughter's mother, who am i to Teresa?",
+        question: "20. If Teresa's daughter is my daughter's mother, who am i to Teresa?",
         options: ["Teresa's Father", "Teresa", "Teresa's son in-law", "Teresa's wife"],
         answer: "Teresa's son in-law"
     },
     {
-        question: "What grows down when it grows up?",
+        question: "21. What grows down when it grows up?",
         options: ["A Butterfly", "A Moth", "A Goose", "An Ant"],
         answer: "A Goose"
     },
     {
-        question: "In what place does today come before yesterday?",
+        question: "22. In what place does today come before yesterday?",
         options: ["Paris", "Bible", "Dictionary", "Space"],
         answer: "Dictionary"
     },
